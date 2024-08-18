d064716c311d43d951100080dbf6f4ef6830f59d
Rahat Muneeb <rahat@pop-os.localdomain>
2024-08-18 11:20:41 +0100

    added netlify adapter and title

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,16 +1,15 @@
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import icon from 'astro-icon';
 
-import icon from 'astro-icon'
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react({
-      // experimentalReactChildren: true,
-    }),
-    tailwind(),
-    icon(),
-  ],
-})
+  integrations: [react({
+    // experimentalReactChildren: true,
+  }), tailwind(), icon()],
+  output: "server",
+  adapter: netlify()
+});
