d0f303cb298f9fa439ceb86496be00a9ac8d910f
Rahat <rahat@pop-os.localdomain>
2025-09-03 23:31:56 +0100

    Add new entries and add sitemap/robots.txt

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -2,14 +2,15 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
-
+import sitemap from '@astrojs/sitemap';
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://dreamteamsingers.co.uk",
   integrations: [react({
     // experimentalReactChildren: true,
-  }), tailwind(), icon()],
+  }), tailwind(), icon(), sitemap()],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
